f76fcd02627ab86158f96c3ab9901a3da99842e2
Nelson Rincon <nerincon1@gmail.com>
2018-03-04 22:33:30 +0000

    Modified with functions

--- guess.py
+++ guess.py
@@ -1,31 +1,41 @@
-# GUESS A NUMBER ------>
+# GUESS A NUMBER ------> Better with OOP
 from random import randint
 
-print("I am thinking of a number betwwen 1 and 10")
-random_number = randint(1,10)
-count = 5
-
-while True:
-    if count > 0:
-        guess = int(input("What's the number? "))
-        count -= 1
-        if guess > random_number:
-            print("{} is too high".format(guess))
-            if count >0:
-                print("You have {} guesses left".format(count))
-        elif guess < random_number:
-            print("{} is too low".format(guess))
-            if count > 0:
-                print("You have {} guesses left".format(count))
+def play_again():
+    while True:
+        play_again = input("Do you want to play again? (y/n) ")
+        if play_again == "y":
+            random_number = randint(1,10)
+            count = 5
+            play_game()
         else:
-            print("You are correct!! You Won!")
-            play_again = input("Do you want to play again? (y/n) ")
-            if play_again == "y":
-                random_number = randint(1,10)
-                count = 5
+            print("Thank you for playing!")
+            quit()
+
+
+
+def play_game():
+    print("I am thinking of a number betwwen 1 and 10")
+    random_number = randint(1,10)
+    count = 5
+    while True:
+        if count > 0:
+            guess = int(input("What's the number? "))
+            count -= 1
+            if guess > random_number:
+                print("{} is too high".format(guess))
+                if count >0:
+                    print("You have {} guesses left".format(count))
+            elif guess < random_number:
+                print("{} is too low".format(guess))
+                if count > 0:
+                    print("You have {} guesses left".format(count))
             else:
-                print("Thank you for playing!")
-                break
-    else:
-        print("You ran out of guesses!")
-        break
+                print("You are correct!! You Won!")
+                play_again()
+    
+        else:
+            print("You ran out of guesses!")
+            play_again()
+
+play_game()
